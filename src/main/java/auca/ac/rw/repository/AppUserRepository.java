66a362262ec4d9aaea24c1d9d94e670b6a69ca10
Tracy Uwase <uwasetracy@120>
2026-03-13 23:17:53 +0200

    latest changes

--- src/main/java/auca/ac/rw/repository/AppUserRepository.java
+++ src/main/java/auca/ac/rw/repository/AppUserRepository.java
@@ -17,44 +17,38 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long>, JpaSpec
 
     AppUser findByUserCodeIgnoreCase(String userCode);
 
-    @Query("""
-            select u from AppUser u
-            join u.village v
-            join v.cell c
-            join c.sector s
-            join s.district d
-            join d.province p
-            where lower(p.code) = lower(:provinceCode)
-            order by u.firstName asc, u.lastName asc
-            """)
+    @Query("select u from AppUser u "
+            + "join u.village v "
+            + "join v.cell c "
+            + "join c.sector s "
+            + "join s.district d "
+            + "join d.province p "
+            + "where lower(p.code) = lower(:provinceCode) "
+            + "order by u.firstName asc, u.lastName asc")
     List<AppUser> findAllByProvinceCode(@Param("provinceCode") String provinceCode);
 
-    @Query("""
-            select u from AppUser u
-            join u.village v
-            join v.cell c
-            join c.sector s
-            join s.district d
-            join d.province p
-            where lower(p.name) = lower(:provinceName)
-            order by u.firstName asc, u.lastName asc
-            """)
+    @Query("select u from AppUser u "
+            + "join u.village v "
+            + "join v.cell c "
+            + "join c.sector s "
+            + "join s.district d "
+            + "join d.province p "
+            + "where lower(p.name) = lower(:provinceName) "
+            + "order by u.firstName asc, u.lastName asc")
     List<AppUser> findAllByProvinceName(@Param("provinceName") String provinceName);
 
-    @Query("""
-            select u from AppUser u
-            join u.village v
-            join v.cell c
-            join c.sector s
-            join s.district d
-            join d.province p
-            where (:provinceCode is null or lower(p.code) = lower(:provinceCode))
-              and (:districtCode is null or lower(d.code) = lower(:districtCode))
-              and (:sectorCode is null or lower(s.code) = lower(:sectorCode))
-              and (:cellCode is null or lower(c.code) = lower(:cellCode))
-              and (:villageCode is null or lower(v.code) = lower(:villageCode))
-            order by u.firstName asc, u.lastName asc
-            """)
+    @Query("select u from AppUser u "
+            + "join u.village v "
+            + "join v.cell c "
+            + "join c.sector s "
+            + "join s.district d "
+            + "join d.province p "
+            + "where (:provinceCode is null or lower(p.code) = lower(:provinceCode)) "
+            + "and (:districtCode is null or lower(d.code) = lower(:districtCode)) "
+            + "and (:sectorCode is null or lower(s.code) = lower(:sectorCode)) "
+            + "and (:cellCode is null or lower(c.code) = lower(:cellCode)) "
+            + "and (:villageCode is null or lower(v.code) = lower(:villageCode)) "
+            + "order by u.firstName asc, u.lastName asc")
     List<AppUser> findAllByLocation(
             @Param("provinceCode") String provinceCode,
             @Param("districtCode") String districtCode,
